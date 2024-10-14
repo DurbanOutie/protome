2f611479351262bc0c04bc3bad90beaa792f21d8
durbanoutie <durbanout13@gmail.com>
2024-10-14 09:51:28 +0200

    Added sample font

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -o protome main.c `sdl2-config --cflags --libs`
+gcc -o protome main.c `sdl2-config --cflags --libs` -lSDL2_ttf
--- main.c
+++ main.c
@@ -1,16 +1,26 @@
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
 
-#define SCR_W 1600
-#define SCR_H 900
+#define SCR_W 800
+#define SCR_H 600
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-
+static TTF_Font *font = NULL;
+static SDL_Surface *text = NULL;
+static SDL_Texture *text_texture = NULL;
 
 static void panic_and_abort(const char *title, const char *message){
     fprintf(stderr, "PANIC: %s ... %s\n", title, message);
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, message, window);
+    TTF_CloseFont(font);
+    TTF_Quit();
+    SDL_DestroyTexture(text_texture);
+    SDL_FreeSurface(text);
     SDL_Quit();
     exit(1);
 }
@@ -30,6 +40,30 @@ int main(int argc, char **argv){
     if(renderer == NULL){
         panic_and_abort("SDL_CreateWindow Failed!", SDL_GetError());
     }
+
+
+    if(TTF_Init() != 0){
+        panic_and_abort("TTF_Init Failed!", TTF_GetError());
+    }
+
+    font = TTF_OpenFont("FreeSans.ttf", 24);
+    if(font == NULL){
+        panic_and_abort("TTF_OpenFont Failed!", TTF_GetError());
+    }
+
+
+    SDL_Color color = { 0, 0, 0, 0};
+
+
+    text = TTF_RenderText_Solid(font, "Hello Ash", color);
+
+    if(text == NULL){
+        panic_and_abort("TTF_OpenFont Failed!", TTF_GetError());
+    }
+
+    text_texture = SDL_CreateTextureFromSurface(renderer, text);
+
+    SDL_Rect dst = { 0, 0, 500, 500};
 
 
     SDL_bool is_running = SDL_TRUE;
@@ -58,12 +92,18 @@ int main(int argc, char **argv){
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
 
+
         //      Draw things
+        SDL_RenderCopy(renderer, text_texture, NULL, &dst);
 
 
         //      Buffer swap
         SDL_RenderPresent(renderer);
     }
+    TTF_CloseFont(font);
+    TTF_Quit();
+    SDL_DestroyTexture(text_texture);
+    SDL_FreeSurface(text);
     SDL_Quit();
     return 0;
 }
